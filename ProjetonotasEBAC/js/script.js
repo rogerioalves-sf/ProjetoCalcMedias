c8f8a89313030c3947d7fc6f3f6fa5a8145628f5
Rogério <146471734+rogerioalves-sf@users.noreply.github.com>
2023-12-12 13:50:55 -0300

    projetonotas

---
+++ ProjetonotasEBAC/js/script.js
@@ -0,0 +1,60 @@
+const form = document.getElementById("form");
+let linhas = '';
+const atividades = [];
+const notas = [];
+const spanAprovado = `<span class="resultado aprovado"> Aprovado </span>`;
+const spanReprovado = `<span class="resultado reprovado"> Reprovado </span>`;
+const notaMin = parseFloat(prompt ('Digite a nota mínima:'));
+
+form.addEventListener('submit',function(e) {
+    e.preventDefault();
+
+    adicionarLinha ();
+    atualizaTabela ();
+    atualizaMediafinal ();
+});
+
+function adicionarLinha () {
+    
+    const inputNomeAtividade = document.getElementById('nome-atividade');
+    const inputNotaAtividade = document.getElementById('nota-atividade');
+
+    if (atividades.includes(inputNomeAtividade.value)) {
+        alert(`A atividade ${inputNomeAtividade.value} já foi inserida`)
+    } else {
+    
+    atividades.push(inputNomeAtividade.value);
+    notas.push(parseFloat(inputNotaAtividade.value))
+
+    let linha = `<tr>`;
+    linha += `<td> ${inputNomeAtividade.value} </td>`;
+    linha += `<td> ${inputNotaAtividade.value} </td>`;
+    linha += `<td class ="bgfeliztriste"> ${inputNotaAtividade.value >= notaMin ? ':)': ':('} </td> `;
+    linha += `</tr>`;
+    linhas += linha;
+
+    }
+
+    inputNomeAtividade.value = '';
+    inputNotaAtividade.value = '';
+}
+
+function atualizaTabela () {
+    const corpotabela = document.querySelector('tbody');
+    corpotabela.innerHTML = linhas;
+}
+
+function atualizaMediafinal () {
+    const mediaFinal = calculaMediaFinal ();
+    document.getElementById('mediafinal').innerHTML = mediaFinal;
+    document.getElementById('mediafinalresultado').innerHTML = mediaFinal >= notaMin ? spanAprovado : spanReprovado;
+}
+
+function calculaMediaFinal () {
+    let somaNotas = 0;
+    for (let i = 0; i < notas.length; i++ ){
+        somaNotas += notas[i];
+    }
+
+    return somaNotas / notas.length;
+}
